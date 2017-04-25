1fad98ee9e030df39c921e3b914b06d0e2774f9e
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-25 16:04:09 +0300

    machines-23

--- wp-content/themes/Msocial_chikurov/template.home.php
+++ wp-content/themes/Msocial_chikurov/template.home.php
@@ -186,7 +186,7 @@ gk_load('header');
           <h2 class="black">Терапевтические программы</h2>
           <div class="col-sm-6 col-xs-12 terapevtic_btns row">
             <!-- Nav tabs -->
-            <div class="col-md-6 terapevtic_btn active"><a href="#home" data-toggle="tab">Wizard Machine</a></div>
+            <div class="col-md-6 terapevtic_btn"><a class="active" href="#home" data-toggle="tab">Wizard Machine</a></div>
             <div class="col-md-6 terapevtic_btn"><a href="#profile" data-toggle="tab">Wizard Duos</a></div>
             <div class="col-md-6 terapevtic_btn"><a href="#messages" data-toggle="tab">Wizard Tarot</a></div>
             <div class="col-md-6 terapevtic_btn"><a href="#settings" data-toggle="tab">Marakata</a></div>
