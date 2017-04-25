370f1070ca9da8d4e9a38de6cebced3017e65eae
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-04-25 16:15:08 +0300

    machines-24

--- wp-content/themes/Msocial_chikurov/template.home.php
+++ wp-content/themes/Msocial_chikurov/template.home.php
@@ -193,7 +193,7 @@ gk_load('header');
           </div>
           <div class="col-sm-6 col-xs-12 pull-right textbox text-center">
               <!-- Tab panes -->
-              <div class="tab-content">
+              <div class="tab-content" style="border-top: 1px solid #ddd;margin-top: 15px;">
                 <div class="tab-pane active" id="home">1</div>
                 <div class="tab-pane" id="profile">2</div>
                 <div class="tab-pane" id="messages">3</div>
