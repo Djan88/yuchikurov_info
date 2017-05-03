77c3ed7303d37d3e3e3c016c4a0b7b61c3dd746a
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-05-03 15:23:09 +0300

    machines-58

--- wp-content/themes/Msocial_chikurov/layouts/header.php
+++ wp-content/themes/Msocial_chikurov/layouts/header.php
@@ -278,14 +278,23 @@
             <span class="white"><i class="fa fa-phone fa-fw" style="margin-right: 5px;"></i>+7 (495) 135-25-48</span>
         </div>
         <div class="consultaiton pull-right">
-            <a href="#" style="color: #f6cd80;" data-toggle="modal" data-target=".bs-example-modal-sm">
+            <a href="#" style="color: #f6cd80;" data-toggle="modal" data-target="#videoCours">
                 <i class="fa fa-play fa-fw" style="margin-right: 5px;"></i>ЗАКАЗАТЬ ВИДЕОКУРС
             </a>
         </div>
-        <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+        <div class="modal fade bs-example-modal-sm" id="videoCours" tabindex="-1" role="dialog" aria-labelledby="videoCoursLabel" aria-hidden="true">
           <div class="modal-dialog modal-sm">
             <div class="modal-content">
-              ...
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="videoCoursLabel">Заказать видеокурс</h4>
+              </div>
+              <div class="modal-body">
+                <?php echo do_shortcode('[contact-form-7 id="3605" title="Заказ видео курса"]')?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+              </div>
             </div>
           </div>
         </div>
