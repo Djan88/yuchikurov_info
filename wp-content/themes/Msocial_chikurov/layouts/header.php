a901cf7ec938e4df0cf3ac259e7ee6d676d00ce6
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-05-03 14:55:48 +0300

    machines-54

--- wp-content/themes/Msocial_chikurov/layouts/header.php
+++ wp-content/themes/Msocial_chikurov/layouts/header.php
@@ -281,22 +281,22 @@
             <a href="#" style="color: #f6cd80;" data-toggle="modal" data-target="#myModal-video">
                 <i class="fa fa-play fa-fw" style="margin-right: 5px;"></i>ЗАКАЗАТЬ ВИДЕОКУРС
             </a>
-            <div class="modal fade" id="myModal-video" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel">Заказать видеокурс</h4>
-                  </div>
-                  <div class="modal-body">
-                    <?php echo do_shortcode('[contact-form-7 id="3605" title="Заказ видео курса"]')?>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-                  </div>
-                </div>
+        </div>
+        <div class="modal fade" id="myModal-video" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="myModalLabel">Заказать видеокурс</h4>
+              </div>
+              <div class="modal-body">
+                <?php echo do_shortcode('[contact-form-7 id="3605" title="Заказ видео курса"]')?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
               </div>
             </div>
+          </div>
         </div>
         <div class="collapse navbar-collapse pull-right">
           <ul class="nav navbar-nav">
