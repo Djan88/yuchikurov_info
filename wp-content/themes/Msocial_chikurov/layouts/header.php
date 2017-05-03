e637d562e607f34bfdb6dc1be7d03aac1a5e6f7b
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-05-03 17:04:23 +0300

    machines-72

--- wp-content/themes/Msocial_chikurov/layouts/header.php
+++ wp-content/themes/Msocial_chikurov/layouts/header.php
@@ -275,11 +275,11 @@
         <div class="consultaiton pull-right">
             <a href="mailto:info@chikurov.com">
             <i class="fa fa-envelope-o fa-fw" style="margin-right: 5px;"></i>info@chikurov.com</a> | 
-            <span class="white"><i class="fa fa-phone fa-fw" style="margin-right: 15px;"></i>+7 (495) 135-25-48</span>
+            <span class="white"><i class="fa fa-phone fa-fw" style="margin-right: 5px;"></i>+7 (495) 135-25-48</span>
         </div>
         <div class="consultaiton pull-right">
           <a href="https://www.youtube.com/user/ThePractik01" target="_blank" style="color: #f6cd80;" data-toggle="modal" data-target="#videoCours">
-            <i class="fa fa-play fa-fw" style="margin-right: 5px;"></i>ЗАКАЗАТЬ ВИДЕОКУРС
+            <i class="fa fa-play fa-fw" style="margin-right: 15px;"></i>ЗАКАЗАТЬ ВИДЕОКУРС
           </a>
         </div>
         <div class="collapse navbar-collapse pull-right">
