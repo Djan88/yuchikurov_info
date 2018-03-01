5228c19011adc2830ea05d910f15ce36ca9fc00d
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-03-01 22:43:15 +0300

    book-1

--- wp-content/themes/Msocial_chikurov/layouts/footer.php
+++ wp-content/themes/Msocial_chikurov/layouts/footer.php
@@ -196,7 +196,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="videoCoursLabel">Скачать книгу "Биологическое центрирование"</h4>
+                <h4 class="modal-title" id="videoCoursLabel">Скачать книгу</h4>
             </div>
             <div class="modal-body">
                 <?php echo do_shortcode('[contact-form-7 id="3799" title="Скачать книгу"]')?>
